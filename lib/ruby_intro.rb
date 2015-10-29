e3db2c7a9c53d1813d9b7310a828cd7419ac61e9
Antonio Alves Melo Junior <antonio@lit.ifce.edu.br>
2015-10-29 23:28:21 +0000

    HW0 done 0

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,33 +3,79 @@
 # Part 1
 
 def sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  count = 0
+  arr.each {|x| count += x} 
+  return count
 end
 
 def max_2_sum arr
-  # YOUR CODE HERE
+  return 0 if arr.empty?
+  return arr[0] if arr.length == 1
+  arr.sort!
+  return arr.pop + arr.pop
 end
 
 def sum_to_n? arr, n
-  # YOUR CODE HERE
+  return false if arr.empty? 
+  return false if arr.length == 1
+  arr.each do |x|
+    if x < n
+      arr.each {|y| return true if x != y && x + y == n }
+    end
+  end
+  return false
 end
 
 # Part 2
 
 def hello(name)
-  # YOUR CODE HERE
+  "Hello, #{name}"
 end
 
 def starts_with_consonant? s
-  # YOUR CODE HERE
+  s.downcase!
+  return false if s.empty?
+  return false if not s.chr =~ /[[:alpha:]]/
+  return false if ["a", "e", "i", "o", "u"].include? s.chr
+  return true
 end
 
 def binary_multiple_of_4? s
-  # YOUR CODE HERE
+  return false if s.empty?
+  return false if s =~ /[[:alpha:][:cntrl:]]/
+  return true if s.to_i % 4 ==  0
 end
 
 # Part 3
 
 class BookInStock
-# YOUR CODE HERE
+  def initialize(isbn, price)
+    raise ArgumentError, 'Argument is not a' if isbn.empty? #=~ /[[:alpha:][:cntrl:]]/
+    raise ArgumentError, 'Argument is not a positive number!' if price <= 0
+    @ISBN = isbn
+    @price = price
+    '''attr_accessor :ISBN
+    attr_accessor :price'''
+  end
+  
+  def price_as_string
+    return "$%.2f" % @price
+  end
+  
+  def isbn
+    @ISBN
+  end
+  
+  def price
+    @price
+  end
+  
+  def isbn=(isbn)
+    @ISBN = isbn
+  end
+  
+  def price=(price)
+    @price = price
+  end
 end
